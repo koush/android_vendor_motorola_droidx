ed758279b0124dc9289c6ceee8906c4b9ac7446f
Koushik Dutta <koushd@gmail.com>
2010-08-17 22:34:14 -0700

    set the device to adb mode so adbd works...

--- postrecoveryboot.sh
+++ postrecoveryboot.sh
@@ -2,6 +2,6 @@
 
 # Restart with root hacked adbd
 busybox kill $(busybox ps | busybox grep adbd)
-echo msc_adb /dev/usb_device_mode
+echo msc_adb > /dev/usb_device_mode
 sync
 /sbin/adbd recovery &
