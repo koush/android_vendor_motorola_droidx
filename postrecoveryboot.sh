fc715c60f38507fea325429b4b1756eb68e796fc
Koushik Dutta <koushd@gmail.com>
2010-08-20 10:31:48 -0700

    wait for system to settle then unmount/remount

--- postrecoveryboot.sh
+++ postrecoveryboot.sh
@@ -1,5 +1,22 @@
 #!/sbin/sh
 
+sleep 5
+
+for i in $(seq 1 10)
+do
+    TMP=$(mount | grep /tmp)
+    if [ -z "$TMP" ]
+    then
+        break
+    fi
+    umount -l /tmp
+    sleep 1
+done
+
+busybox mount -orw,remount /
+rm -r /tmp
+mkdir -p tmp
+
 # Restart with root hacked adbd
 busybox kill $(busybox ps | busybox grep adbd)
 echo msc_adb > /dev/usb_device_mode
